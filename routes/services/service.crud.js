8b80ce9696ec5b92a0ab661e60d90ac89440dab6
Jyoti Kamal Singh <124129047+Jyoti-Metapercept@users.noreply.github.com>
2023-07-23 10:50:34 +0530

    new routes added

--- routes/services/service.crud.js
+++ routes/services/service.crud.js
@@ -3,6 +3,6 @@ const serviceController = require("../../controller/services/services.crud.js");
 const router = Router();
 
 router.post("/service/addService", serviceController.addServices);
-router.get("service/getServices",serviceController.getServices)
+router.get("/service/getServices",serviceController.getServices)
 
 module.exports = router;
